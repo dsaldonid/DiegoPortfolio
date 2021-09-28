71968a4954e1ca4cf363abf635df57ae00b4cca4
Diego Saldonid <dsaldoni@ucsd.edu>
2021-09-28 08:05:12 -0700

    set up template

---
+++ frontend/portfolio/src/App.js
@@ -0,0 +1,15 @@
+import AboutMe from "./Components/AboutMe/AboutMe"
+import Navbar from "./Components/Navbar/Navbar"
+import Hero from "./Components/Hero/Hero"
+import Projects from "./Components/Projects/Projects"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/portfolio/src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import { Box, Button } from "@material-ui/core"
+export default function Navbar() {
+    return (
+        <Box>
+            <Box>
+                <Button>About Me</Button>
+                <Button>Projects</Button>
+                <Button>Resume</Button>
+            </Box>
+
+        </Box>
+    )
+}
